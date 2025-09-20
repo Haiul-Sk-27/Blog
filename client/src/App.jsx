cad46cd445fc62221ef105b2a00dcc1dcb2550e5
Haiul sk <HaiulSk037@Gmail.com>
2025-09-20 08:32:03 +0530

    Blog_Client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-gray-600 text-left'>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
