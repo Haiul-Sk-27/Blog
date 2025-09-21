7369fbdacb1e8cb8e2cbac0dc582fb3e26d78781
Haiul sk <HaiulSk037@Gmail.com>
2025-09-21 20:33:03 +0530

    db

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async() =>{
     try{
-        await mongoose.connect(process.env.MONGO_URl)
+        await mongoose.connect(process.env.MONGO)
         console.log("MongoDB Connected Successfully!")
     }catch(error){
         console.log("MongoDb Connection Error")
