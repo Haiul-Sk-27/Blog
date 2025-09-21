0c4a6761c3faf0dd17c2bf213d20c773188510b1
Haiul sk <HaiulSk037@Gmail.com>
2025-09-21 08:03:57 +0530

    Blog_Client

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
     const user = false;
     return (
-        <div className='py-2 dark:bg-gray-800 dark:border-b-gray-600 border-b-gray-300 border-2 bg-white z-50'>
+        <div className='py-2 fixed w-full dark:bg-gray-800 dark:border-b-gray-600 border-b-gray-300 border-2 bg-white z-50'>
             <div className='max-w-7xl mx-auto flex justify-between items-center px-4 md:px-0'>
                 {/*logo section*/}
                 <div className='flex gap-7 items-center'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import Hero from '@/components/Hero'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>Home
-        <Button></Button>
+        <Hero/>
     </div>
   )
 }
