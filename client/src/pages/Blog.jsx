4c86c2a4616b2d01e9435c4cbf21603a9c53762a
Haiul sk <HaiulSk037@Gmail.com>
2025-10-02 16:09:14 +0530

    Blogs

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,7 +4,6 @@ import LMS from "../assets/LMS.png"
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { setBlog } from '@/redux/blogSlice'
-// import BlogCardList from '@/components/BlogCardList'
 
 export const blogJson = [
     {
@@ -81,7 +80,7 @@ const Blog = () => {
     },[])
 
     return (
-        <div className='pt-16'>
+        <div className='pt-16 h-screen'>
             <div className='max-w-6xl mx-auto text-center flex flex-col space-y-4 items-center sticky top-14.5 z-50'>
                 <h1 className='text-4xl font-bold text-center pt-10 '>Our Blogs</h1>
                 <hr className=' w-24 text-center border-2 border-red-500 rounded-full' />
