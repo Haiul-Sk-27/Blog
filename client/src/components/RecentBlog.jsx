4a7c94ce6447f8db3198728eb2a80e041d493f39
Haiul Sk <haiulsk037@gmail.com>
2025-10-03 14:53:03 +0530

    RecentBLog

--- client/src/components/RecentBlog.jsx
+++ client/src/components/RecentBlog.jsx
@@ -3,6 +3,9 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BlogCardList from './BlogCardList';
+import { Badge } from "@/components/ui/badge"
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 const RecentBlog = () => {
     const dispatch = useDispatch();
@@ -34,11 +37,46 @@ const RecentBlog = () => {
             <div className='max-w-7xl mx-auto flex gap-6'>
                 <div>
                     <div className='mt-10 px-4 md:px-0'>
-                         {
+                        {
                             blog?.slice(0, 4)?.map((blog, index) => {
                                 return <BlogCardList key={index} blog={blog} />
                             })
                         }
+                    </div>
+
+                </div>
+                <div className='bg-white hidden md:block dark:bg-gray-700 w-[350px] p-5 rounded-md mt-10'>
+                    <h1 className='text-2xl font-semibold'>Populat Categories</h1>
+                    <div className='my-5 flex flex-wrap gap-3'>
+                        {
+                            ["Blogging", "Web Development", "Digital Marketing", "Cooking", "Photography", "Sports"].map((iteam, index) => {
+                                return <Badge className="cursor-pointer">{iteam}</Badge>
+                            })
+                        }
+                    </div>
+                    <h1 className='text-xl font-semibold'>Sucribe Newsletter</h1>
+                    <p className='text-sm text-gray-600 dark:text-gray-400'>Get the latest posts and updates delivered straight to your inbox.</p>
+                    <div className='flex flex-col sm:flex-row gap-2 max-w-md mx-auto mt-5'>
+                        <Input type="email" placeholder="Enter your email" className="flex h-10 w-full rounded-md border bg-gray-200 dark:bg-gray-800 px-3 py-2 text-sm  text-gray-300" />
+                        <Button>Subcribe</Button>
+                    </div>
+                    <div className='mt-7'>
+                        <h2 className='text-xl font-semibold mb-3'>SUggested Blogs</h2>
+                        <ul className='space-y-3'>
+                            {
+                                [
+                                    '10 Tips to Master React',
+                                    'Understanding Tailwind CSS',
+                                    'Improve SEO in 2024',
+                                ].map((title, idx) => (
+                                <li
+                                    key={idx}
+                                    className="text-sm dark:text-gray-100  hover:underline cursor-pointer"
+                                >
+                                    {title}
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
